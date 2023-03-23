4ac9a5805ce5c4d7822b0e96d412ac9ccfb745c3
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-23 17:52:14 +0100

    add: class product and category

--- index.php
+++ index.php
@@ -1,27 +1,44 @@
-<!-- Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+<?php
+// Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
 
-L'e-commerce vende **prodotti** per animali.
+//  TODO L'e-commerce vende ** prodotti ** per animali.
 
-I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
+// I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
 
-I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
+// I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
 
-Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
-prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
-
-
-<?php 
+// Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
+// prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). 
 
 
+class Category
+{
+    public $type;
+
+    public function __construct(string $_type)
+    {
+        $this->type = $_type;
+    }
+}
+
+class Product
+{
+    public $image;
+    public $title;
+    public $price;
+    public $category;
 
 
+    public function __construct(string $_image, string $_title, float $_price, Category $_category)
+    {
+        $this->image = $_image;
+        $this->title = $_title;
+        $this->price = $_price;
+        $this->category = $_category;
+    }
+}
 
 
+$test = new Product("link img", "Osso", 4.21, new Category("Cane"));
 
-
-
-
-
-
-
-?>
+var_dump($test);
